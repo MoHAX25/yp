b82c35c3bc207721871aab585efdbfca21015a76
Undefined <46818324+MoHAX25@users.noreply.github.com>
2026-07-13 22:16:57 +0300

    first commit

---
+++ IEventService.cs
@@ -0,0 +1,21 @@
+﻿namespace yp
+{
+    using System;
+    using System.Collections.Generic;
+    using Models;
+
+    public interface IEventService
+    {
+        IEnumerable<Event> GetAll();
+        Event? Get(Guid id);
+        void Create(Event ev);
+        /// <summary>
+        /// Обновляет событие по id. Возвращает true, если событие найдено и обновлено.
+        /// </summary>
+        bool Update(Guid id, Event ev);
+        /// <summary>
+        /// Удаляет событие по id. Возвращает true, если событие найдено и удалено.
+        /// </summary>
+        bool Delete(Guid id);
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace yp.Models
+{
+    public class Event
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public DateTime StartAt { get; set; }
+        [Required]
+        public DateTime EndAt { get; set; }
+
+    }
+}
