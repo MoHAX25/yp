a7ee86def78735180b29b263fce178331876442b
Undefined <46818324+MoHAX25@users.noreply.github.com>
2026-07-14 10:41:30 +0300

    add swagger

--- Program.cs
+++ Program.cs
@@ -6,14 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IEventService, EventService>();
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "YP API V1");
+});
 
 app.UseHttpsRedirection();
 
